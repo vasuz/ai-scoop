1e1b60f9339b58aae9a7408529caec9f766c01af
Vasu <29741584+vasuz@users.noreply.github.com>
2021-09-18 19:33:02 -0400

    URL validation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import flask
 from flask import request, jsonify
+
 import model
+from validators import url as valid_url
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,7 +10,8 @@ app.config["DEBUG"] = True
 @app.route('/api/go', methods=["POST"])
 def go():
     url = request.args.get('url')
-    if url:
+
+    if valid_url(url):
         return jsonify(model.process(url))
     else:
         return jsonify("Please provide a valid URL.")
