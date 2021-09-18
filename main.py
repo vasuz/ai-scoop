fb2f6a35609779740f9c7e4992e35100ae192577
Vasu <29741584+vasuz@users.noreply.github.com>
2021-09-18 19:22:03 -0400

    Refactor api->model flow

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import flask
 from flask import request, jsonify
+import model
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/api/go')
+@app.route('/api/go', methods=["POST"])
 def go():
-    return jsonify({
-        "hello": "world"
-    })
+    print(request)
+    return jsonify(model.process(""))
 
 app.run()
---
+++ model.py
@@ -0,0 +1,6 @@
+def process(url):
+    return {
+        "summary": None,
+        "cw_puzzle": "imageurlhere",
+        "ws_puzzle": "imageurlhere"
+    }
