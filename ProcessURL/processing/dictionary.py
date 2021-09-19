cbf1d30b04791cee90a23ee6c366b93e822cc3d1
qinjoshua <qinjoshuac@outlook.com>
2021-09-19 01:11:30 -0400

    Added dictionary functionality

---
+++ ProcessURL/processing/dictionary.py
@@ -0,0 +1,16 @@
+import requests
+import json
+from utils import get_config
+
+def get_definition_merriam(word):
+    # This should be fetched from some appsettings file
+    key = get_config("merriam-webster-api-key");
+    url = "https://dictionaryapi.com/api/v3/references/sd2/json/" + word + "?key=" + key;
+
+    response = requests.get(url);
+
+    # We hardcode this to get the top response. It's possible another definition is necessary?
+    data = json.loads(response.content)[0];
+
+    definition = "\n".join(data["shortdef"]);
+    return definition;
---
+++ ProcessURL/processing/utils.py
@@ -0,0 +1,8 @@
+import json
+
+CONFIG_FILE = "appconfig.json"
+
+def get_config(option):
+    with open(CONFIG_FILE) as json_file:
+        data = json.load(json_file);
+        return(option)
